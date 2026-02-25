d924e500fef2bafb04188c79cfec6bd290cee3a1
JulesJohnstone23 <jules213@proton.me>
2026-02-26 09:47:09 +1300

    Addition: term deposit basic functionality and contacts added

--- bankingApp/bankingApp.cpp
+++ bankingApp/bankingApp.cpp
@@ -80,7 +80,9 @@ class Bank{
         std::vector<std::string> savedContacts={};
         std::string password;
         double termDepositBalance=0.0;
-
+        double effectiveTermDepositBalance=0.0;
+        int termDepositTime=0;
+        
     
     public:
 
@@ -89,24 +91,192 @@ class Bank{
             this->bankBalance= bankBalance;
             this->interestRate = interestRate;
             this->savedContacts=savedContacts;
+            if(savedContacts.size()==0){
+                std::ifstream reader("savedContacts.txt");
+                bool found = false;
+                if(reader.is_open()){
+                    std::string line;
+                    while(std::getline(reader,line)){
+                                
+                        std::string currentContact="";
+                        if(line.find(username)!=std::string::npos){
+                            
+                            for(auto ch: line){
+                                if(ch==':'){
+                                    found=true;
+                                }
+                                else if(found){
+                                    if(ch!=','){
+                                        currentContact=currentContact+ch;
+                                    }
+                                    else{
+                                        savedContacts.push_back(currentContact);
+                                        currentContact="";
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+
+                }
+                reader.close();
+                if(!found){
+
+                    std::ifstream reader("savedContacts.txt");
+                    bool found = false;
+                    if(reader.is_open()){
+                        found=true;
+                    }
+                    else{
+                        found=false;
+                    }
+                    reader.close();
+
+                    std::ofstream writer;
+                    writer.open("savedContacts.txt",std::ios::app);
+
+                    if(!writer){
+                        std::cout<<"error opening file"<<std::endl;
+                    }
+
+                    if(found){
+                        writer<<'\n'<<username<<":"<<'\n';
+
+                        
+                    }
+                    else{
+                        writer<<username<<":"<<'\n';
+                    }
+                    writer.close();
+
+                }
+            }
+            
         }
+
+        
 
         void checkBalance(){
             std::cout<<"You're balance is: $"<<bankBalance<<std::endl;
         }
 
-        void checkTermDeposit(){
+        void checkAllContacts(){
+            std::cout<<"Saved contact size: "<<savedContacts.size()<<std::endl;
+            std::cout<<"The following are your contacts"<<std::endl;
+            for(auto contact : savedContacts){
+                std::cout<<contact<<std::endl;
+            }
+            
 
-            if(termDepositBalance==0.0){
+        }
+
+        void makeTermDeposit(){
+            double userInputTermDeposit=0.0;
+            bool running = true;
+            while(running){
+                std::cout<<"How much money from your balance would you like to dedicate towards a term deposit?"<<std::endl;
+                std::cin >> userInputTermDeposit;
+                if(userInputTermDeposit<=bankBalance){
+                    bankBalance = bankBalance - termDepositBalance;
+                    termDepositBalance=userInputTermDeposit;
+                    running = false;
+                }
+                else{
+                    std::cout<<"Please enter a valid amount OR make sure you have enough money in the bank"<<std::endl;
+                }
+            }
+            running = true;
+            std::cout<<"What interest rate would you like to have?(as a %)"<<std::endl;
+            std::cin >> interestRate;
+            std::cout<<"And for how long? (in months)"<<std::endl;
+            std::cin>>termDepositTime;
+
+            std::ofstream writer;
+            writer.open("termDeposit.txt",std::ios::app);
+
+            if(!writer){
+                std::cout<<"error opening file"<<std::endl;
+            }  
+
+            time_t timeDate;
+            time(&timeDate);
+            struct tm *localTime = localtime(&timeDate);
+            int year = localTime->tm_year+1900;
+            int month = localTime->tm_mon +1;
+            int day = localTime->tm_mday;
+            
+
+
+            writer<<username<<":"<<termDepositBalance<<","<<interestRate<<","<<termDepositTime<<","<<year<<","<<month<<","<<day<<","<<'\n';
+            writer.close();
+
+
+        }
+
+        void checkTermDeposit(){
+            if(termDepositBalance==0){
                 std::cout<<"You do not have a term deposit"<<std::endl;
             }
             else{
-                std::cout<<"You're current term deposit value is"<< termDepositBalance<<std::endl;
+                std::cout<<"You're effective term deposit value is: " <<effectiveTermDepositBalance<<std::endl;
+                std::cout<<"You're current term deposit actual value is: "<<termDepositBalance<<std::endl;
             }
+
 
         }
 
         
+        void addNewContact(){
+
+            std::string newContact="";
+            std::cout<<"What new contact would you like to add?"<<std::endl;
+            std::cin >> newContact;
+
+
+            savedContacts.push_back(newContact);
+            std::cout<<"Saved contact size: "<<savedContacts.size()<<std::endl;
+
+            if(savedContacts.back()==newContact){
+                std::cout<<"Contact has been succesfully added"<<std::endl;
+                std::vector<std::string> savedContactContent;
+                std::ifstream reader("savedContacts.txt");
+                bool newAdded=false;
+                if(reader.is_open()){
+                    std::string line;
+
+                    while(std::getline(reader,line)){
+                            
+                            
+                        if(line.find(username)!=std::string::npos){
+                            savedContactContent.push_back(line+newContact+",");
+                            newAdded=true;
+                        }
+                        else{
+                            savedContactContent.push_back(line);
+                        }
+                    }
+                }
+                reader.close();
+                if(newAdded){
+                    std::ofstream writer("savedContacts.txt");
+
+                    if(!writer){
+                        std::cout<<"error opening file"<<std::endl;
+                    }
+                        
+                    for(auto current : savedContactContent){
+                        writer<<current<<'\n';
+                    }
+                    writer.close();
+                        
+                }
+
+            }
+            
+            else{std::cout<<"Contact has not been succesfully added, try again"<<std::endl;}
+
+        }
 
 
 };
@@ -146,33 +316,41 @@ void loggedIn(std::string username, std::string password){
 
     bool running = true;
     while (running){
+        
         std::string userInput="";
-        std::cout<<"You are now logged in"<<'\n'<<"What would you like to do?"<<'\n'<<"    - Check Balance"<<'\n'<<"    - "<<'\n'<<"    - Deposit"<<'\n'<<"    - Check Term deposit"<<'\n'<<"    - Make term deposit"<<'\n'<<"    - Make a payment"<<'\n'<<"    - Check all contacts"<<'\n'<<"    - Add new Contact"<<std::endl;
-        std::cin >> userInput;
+        std::cout<<"You are logged in"<<'\n'<<"What would you like to do?"<<'\n'<<"    - Check Balance"<<'\n'<<"    - Deposit"<<'\n'<<"    - Check Term deposit"<<'\n'<<"    - Make term deposit"<<'\n'<<"    - Make a payment"<<'\n'<<"    - Check all contacts"<<'\n'<<"    - Add new Contact"<<std::endl;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        std::getline(std::cin, userInput);
+        userInput.erase(std::remove(userInput.begin(), userInput.end(), ' '), userInput.end());  
         std::transform(userInput.begin(), userInput.end(),userInput.begin(),[](unsigned char c){return std::tolower(c);});
-        userInput.erase(std::remove(userInput.begin(), userInput.end(), ' '), userInput.end());        
+       
         Outputs userChoice = convert(userInput);
 
         switch(userChoice){
-
-
             case checkbalance:
                 user.checkBalance();
                 break;
             case deposit:
                 break;
             case checktermdeposit:
+                user.checkTermDeposit();
                 break;
             case maketermdeposit:
+                user.makeTermDeposit();
                 break;
             case makeapayment:
+                
                 break;
             case checkallcontacts:
+                user.checkAllContacts();
                 break;
             case addnewcontact:
+                user.addNewContact();
                 break;
             
         }
+
 
         //srand(time(0));
         //double randomNum = (double)rand()/0.05;
@@ -187,11 +365,12 @@ int main(){
 
     clear();
     bool running = true;
-
+    std::string username="";
+    std::string userPassword;
     while (running){
         std::string userInput="";
         std::cout<<"Welcome to the banking app."<<'\n'<<"Please type one of the following options"<<'\n'<<"    - Login"<<'\n'<<"    - Sign up"<<std::endl;
-        getline(std::cin, userInput);
+        std::getline(std::cin, userInput);
 
         std::transform(userInput.begin(), userInput.end(),userInput.begin(),[](unsigned char c){return std::tolower(c);});
 
@@ -199,7 +378,7 @@ int main(){
             bool usernameFound = false;
             bool finished= false;
             std::string password;
-            std::string username="";
+            
 
                 while(!usernameFound || !finished){
         
@@ -258,7 +437,7 @@ int main(){
                 
         
 
-                std::string userPassword;
+                
                 std::cout<<"Please enter your password"<<std::endl;
                 
 
@@ -267,17 +446,19 @@ int main(){
                 enableEcho();
                 
                 if(userPassword==password){
-
-                    loggedIn(username,userPassword);
+                    running = false;
+                    break;
+                    
 
 
                 }
-                else{
+                if(userPassword!=password){
                     std::cout<<"PLease try again"<<std::endl;
                     usernameFound=false;
                     finished=false;
                 }
         }
+    
         
         else if(userInput=="sign up"){
             srand(time(NULL));
@@ -314,8 +495,10 @@ int main(){
 
 
         } 
+    
 
-    }   
+    }  
+    loggedIn(username,userPassword); 
 
 
     return 0;
